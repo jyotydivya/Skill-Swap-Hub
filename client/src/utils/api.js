7e753841792ac7d0c8ed171ba89e091cf528335e
Divya Jyoty <jyotydivya844@gmail.com>
2026-04-04 10:27:00 +0530

    Use environment variable for API base URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL || '/api' });
 
 // Attach JWT to every request
 api.interceptors.request.use((config) => {
